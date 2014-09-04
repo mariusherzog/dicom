b2c162bad6893cecde96d2d93a33d8db4d08c06f
Marius Herzog <info@mariusherzog.net>
2014-09-04 12:54:36 +0200

    corrected scx and its subclasses, added documentation

--- upperlayer.hpp
+++ upperlayer.hpp
@@ -9,20 +9,28 @@
 
 #include "upperlayer_properties.hpp"
 
+
+/**
+ * The upperlayer namespace contains classes and functions which implement the upperlayer as
+ * described in the DICOM standard chapter 8.
+ * @link http://medical.nema.org/Dicom/2011/11_08pu.pdf
+ */
 namespace upperlayer
 {
 
 
 /**
- * @brief The dicom_upperlayer class implements the Upper Layer as described
- *        in the DICOM3 standard, part 8.
- * @link http://medical.nema.org/Dicom/2011/11_08pu.pdf
- * @todo -currently only as a SCP <BR>
+ * @brief The scx class implements basic functionality used both by the specialed scp and scu
+ *        subclasses, like reading and writing to the connected peer. It also manages the
+ *        state machine
+ * @todo -refactor state machine from class
  *       -extend state machine by release collision states and transitions <BR>
  *       -ARTIM timer <BR>
  *       -
  *
- * It must be noted that the sole responsibility of the upperlayer-scp is to
+ * upperlayer::scx provides send() and read() functions independetly if the
+ * subclass for clients (scu) or servers (scp) is used.
+ * It must be noted that the sole responsibility of the upperlayer::scx is to
  * keep the state machine valid. It is not involved in the association
  * negotiation. This has to be done by the user of this class (either a facade
  * or the DIMSE_PM).
@@ -55,38 +63,75 @@ class scx
       explicit scx();
       virtual ~scx() = 0;
 
+      /**
+       * @brief sock is used by send() and receive() to access the socket of the
+       *        subclasses
+       * @return ref to boost::asio::ip::tcp::socket of the subclass
+       *
+       * for initalization reasons the socket cannot be declared in this abstract
+       * class.
+       */
+      virtual boost::asio::ip::tcp::socket& sock() = 0;
+
+      /**
+       * @brief send takes a property and uses its property::make_pdu() function
+       *        for serialization. The serialized data is sent to the peer via
+       *        the socket.
+       * @param p
+       */
       void send(property* p);
+
+      /**
+       * @brief receive waits for the peer to send data. The received, serialized data
+       *        is converted to a structured representation. The caller receives
+       *        ownership of that property instance.
+       * @return unique_ptr to property
+       */
       std::unique_ptr<property> receive();
 
+      /**
+       * @brief get_state returns the current state
+       * @return
+       */
+      CONN_STATE get_state();
 
 
    private:
       CONN_STATE state;
-
-   protected:
-      boost::asio::io_service io_service;
-      boost::asio::ip::tcp::socket sock;
 };
 
+/**
+ * @brief The scp class acts as a service class provider
+ */
 class scp: public scx
 {
    public:
       scp(short port);
+      ~scp() override;
+      boost::asio::ip::tcp::socket& sock() override;
 
    private:
+      boost::asio::io_service io_service;
+      boost::asio::ip::tcp::socket socket;
       boost::asio::ip::tcp::acceptor acptr;
 };
 
+/**
+ * @brief The scu class acts as a service class user
+ */
 class scu: public scx
 {
    public:
       scu(std::string host, std::string port);
+      ~scu() override;
+      boost::asio::ip::tcp::socket& sock() override;
 
    private:
+      boost::asio::io_service io_service;
       boost::asio::ip::tcp::resolver resolver;
       boost::asio::ip::tcp::resolver::query query;
       boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-      boost::asio::ip::tcp::resolver::iterator end;
+      boost::asio::ip::tcp::socket socket;
 };
 
 
