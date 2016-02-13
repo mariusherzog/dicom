b22d7c1b60dc525df0a904934b5538c1fdb42181
Marius Herzog <info@mariusherzog.net>
2016-02-13 15:44:19 +0100

    make ctor protected

--- source/data/dataset/transfer_processor.hpp
+++ source/data/dataset/transfer_processor.hpp
@@ -23,7 +23,7 @@ namespace dataset
 {
 
 /**
- * @brief The Itransfer_processor struct defines the interface for serializing
+ * @brief The transfer_processor class defines the interface for serializing
  *        and deserializing attribute sets (IODs), honoring the transfer
  *        syntax
  * This abstraction shall be mapped by the DIMSE layer to each transfer syntax
@@ -32,24 +32,69 @@ namespace dataset
 class transfer_processor
 {
    protected:
+      /**
+       * @brief The VR_TYPE enum defines if an explicit VR is used in the
+       *        transfer syntax.
+       */
       enum class VR_TYPE
       {
          IMPLICIT, EXPLICIT
       };
 
-   public:
+      /**
+       * @brief transfer_processor constructor
+       * @param dict optional dictionary, only necessary for implicit VR TSs
+       * @param tfs transfer syntax UID
+       * @param vrtype enum instance defining if the ts is [ex|im]plicit VR.
+       */
       transfer_processor(boost::optional<dictionary::dictionary_dyn&> dict, std::string tfs, VR_TYPE vrtype);
 
+   public:
+      /**
+       * @brief serialize is used to serialize an iod / command set
+       * @param data structured dataset to be serialized
+       * @return bytestream of serialized data
+       */
       std::vector<unsigned char> serialize(iod data) const;
+
+      /**
+       * @brief deserialize shall be called to deserialize a datastream into
+       *        a structured attribute
+       * @param data datastream
+       * @return structured attribute
+       */
       iod deserialize(std::vector<unsigned char> data) const;
+
+      /**
+       * @brief get_transfer_syntax is used to acquire the UID of the classe's
+       *        implemented transfer syntax.
+       * @return UID of the transfer syntax
+       */
       std::string get_transfer_syntax() const;
 
       virtual ~transfer_processor();
 
    private:
+      /**
+       * @brief serialize_attribute is overriden by a subclass to implement
+       *        transfer-syntax specific serialization of data.
+       * @param e attribute
+       * @param vr VR of the attribute
+       * @return vector of bytes with serialized data
+       */
       virtual std::vector<unsigned char>
       serialize_attribute(attribute::elementfield e, attribute::VR vr) const = 0;
 
+      /**
+       * @brief deserialize_attribute is overriden by a subclass to implement
+       *        transfer-syntax specific deserialization of data.
+       * @param data array containing serialized bytes
+       * @param tag tag of the attribute
+       * @param len length of the attribute's data field
+       * @param vr VR of the attribute
+       * @param pos starting position of the data field in the serialized array
+       * @return elementfield with structured data
+       */
       virtual attribute::elementfield
       deserialize_attribute(std::vector<unsigned char>& data,
                             attribute::elementfield::tag_type tag,
