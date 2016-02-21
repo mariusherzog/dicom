c5ae72df413166b955390bc6a1f0e329142204df
Marius Herzog <info@mariusherzog.net>
2016-02-21 11:28:50 +0100

    do not generate identifiers with same name but different tags

--- tools/gentagconstants.sh
+++ tools/gentagconstants.sh
@@ -4,8 +4,12 @@
 # the command or data dictionary. Manual adjustments after
 # creation may be necessary.
 
+declare -A identifiers
+
+
 function write_tag_constant {
    echo "const elementfield::tag_type $3 {$1, $2};"
+   identifiers[$3]=1
 }
 
 
@@ -14,8 +18,8 @@ if [ $# -lt 2 ]; then
    echo "Usage: gendcommanddic.sh commanddic.csv datadic.csv"
 fi
 
-cat tagconstants_template.cpp
 
+cat tagconstants_template.cpp
 
 for file in $(echo "$1 $2"); do
    while read line; do
@@ -27,7 +31,12 @@ for file in $(echo "$1 $2"); do
 
       KEYWCLEAN=$(echo $KEYW | sed -e 's/[^a-zA-Z0-9]//g')
    
-      write_tag_constant "$GID" "$EID" "$KEYWCLEAN" 
+      if [[ $(echo $GID | sed -e 's/^0x//' | tr -cd 'x'| wc -c) -eq 0 ]] \
+         && [[ $(echo $EID | sed -e 's/^0x//' | tr -cd 'x'| wc -c) -eq 0  ]] \
+         && [[ ${identifiers[$KEYWCLEAN]} -ne 1 ]]; then
+
+         write_tag_constant "$GID" "$EID" "$KEYWCLEAN" 
+      fi
    
    done < $file
 done
