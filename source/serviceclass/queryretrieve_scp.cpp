5508eebc5aeaffc153e9ffefb7292bcda726ba6e
Marius Herzog <info@mariusherzog.net>
2017-03-23 19:34:25 +0100

    clean up q/r

--- source/serviceclass/queryretrieve_scp.cpp
+++ source/serviceclass/queryretrieve_scp.cpp
@@ -18,38 +18,6 @@ using namespace dicom::network;
 using handlermap = std::map<dicom::data::dataset::DIMSE_SERVICE_GROUP,
 std::function<void(dicom::network::dimse::dimse_pm* pm, dicom::data::dataset::commandset_data cdata, std::unique_ptr<dicom::data::dataset::iod> data)>>;
 
-dimse::SOP_class findrq {"1.2.840.10008.5.1.4.31",
-{ { dataset::DIMSE_SERVICE_GROUP::C_MOVE_RQ,
-   [](dimse::dimse_pm* pm, dataset::commandset_data command, std::unique_ptr<dataset::iod> data) {
-
-      std::cout << "We have a C-MOVE Request\n";
-      dataset::dataset_type dat, dat2, dat3;
-      dataset::iod seq;
-//      dat[dicom::data::attribute::Item] = dicom::data::attribute::make_elementfield<VR::NI>(0xffffffff);
-//      dat[{0x0008, 0x0104}] = dicom::data::attribute::make_elementfield<VR::LO>(4, "meo");
-//      dat[{0xfffe, 0xe00d}] = dicom::data::attribute::make_elementfield<VR::NI>();
-//      dat2[dicom::data::attribute::Item] = dicom::data::attribute::make_elementfield<VR::NI>(0xffffffff);
-//      dat2[{0x0008, 0x0104}] = dicom::data::attribute::make_elementfield<VR::LO>(4, "mwo");
-//      dat2[{0xfffe, 0xe00d}] = dicom::data::attribute::make_elementfield<VR::NI>();
-//      dat3[dicom::data::attribute::SequenceDelimitationItem] = dicom::data::attribute::make_elementfield<VR::NI>();
-//      seq[{0x0032, 0x1064}] = dicom::data::attribute::make_elementfield<VR::SQ>(0xffffffff, {dat, dat2, dat3});
-
-
-
-//      while () {
-//         // C-Store
-//         auto request_property = ascdef.get_initial_request();
-//         dicom::network::upperlayer::scu sc(dict, "localhost", "11113", request_property);
-//         dicom::network::dimse::dimse_pm_manager dpm(sc, ascdef, dict);
-//         sc.run();
-//         // end c store
-
-//         pm->send_response({dataset::DIMSE_SERVICE_GROUP::C_MOVE_RSP, command, boost::none, });
-//      }
-   }}}
-};
-
-
 
 queryretrieve_scp::queryretrieve_scp(std::string calling_ae, std::string called_ae,
                                      int max_message_len, dicom::data::dictionary::dictionary& dict,
@@ -78,7 +46,7 @@ void queryretrieve_scp::cstore_callback(storage_scu* st, dicom::data::dataset::c
    std::cout << "##sent##\n";
 }
 
-void queryretrieve_scp::handle_cfind(dimse::dimse_pm *pm, dataset::commandset_data command, std::unique_ptr<dataset::iod> data)
+void queryretrieve_scp::handle_cfind(dimse::dimse_pm* pm, dataset::commandset_data command, std::unique_ptr<dataset::iod> data)
 {
    handler(this, command, std::move(data));
 
@@ -104,16 +72,8 @@ void queryretrieve_scp::handle_cfind(dimse::dimse_pm *pm, dataset::commandset_da
 
    //st.send_next_request(resp);
 
-   storage_thread.reset(new std::thread {[this, command, pm]()
-                                         {
-                                            storage_scu st {"QRSCP", "QRSCU", 4096, dict, [this, command, pm](storage_scu* st, dicom::data::dataset::commandset_data cmd, std::unique_ptr<dicom::data::dataset::iod> data) {
-                                                               std::cout << "#sent#" << std::flush;
-                                                               pm->send_response({dicom::data::dataset::DIMSE_SERVICE_GROUP::C_MOVE_RSP, command, boost::none, 0xff00});
-                                                            }
-                                            };
-                                            st.get_scu().run();
-                                         }
-                        });
+   storage_operation.reset(new storage_scu_thread {*this, command, pm, dict});
+   storage_thread.reset(new std::thread {*storage_operation});
 
 
 //      pm->send_response({dicom::data::dataset::DIMSE_SERVICE_GROUP::C_MOVE_RSP, command, boost::none, 0x00ff});
--- source/serviceclass/queryretrieve_scp.hpp
+++ source/serviceclass/queryretrieve_scp.hpp
@@ -12,6 +12,42 @@
 #include "network/dimse/association_definition.hpp"
 #include "storage_scu.hpp"
 
+class queryretrieve_scp;
+
+class storage_scu_thread
+{
+   public:
+      storage_scu_thread(queryretrieve_scp& qr_scp,
+                         dicom::data::dataset::commandset_data cmove_cmd,
+                         dicom::network::dimse::dimse_pm* cfind_pm,
+                         dicom::data::dictionary::dictionary& dict):
+         qr_scp {qr_scp},
+         cmove_cmd {cmove_cmd},
+         cfind_pm {cfind_pm},
+         dict {dict}
+      {
+      }
+
+      void operator()()
+      {
+         storage_scu st
+         {
+            "QRSCP", "QRSCU", 4096, dict,
+                  [this](storage_scu* st, dicom::data::dataset::commandset_data cmd, std::unique_ptr<dicom::data::dataset::iod> data)
+                  {
+                     cfind_pm->send_response({dicom::data::dataset::DIMSE_SERVICE_GROUP::C_MOVE_RSP, cmove_cmd, boost::none, 0xff00});
+                  }
+         };
+         st.get_scu().run();
+      }
+
+   private:
+      queryretrieve_scp& qr_scp;
+      dicom::data::dataset::commandset_data cmove_cmd;
+      dicom::network::dimse::dimse_pm* cfind_pm;
+      dicom::data::dictionary::dictionary& dict;
+};
+
 class queryretrieve_scp
 {
    public:
@@ -40,6 +76,7 @@ class queryretrieve_scp
       dicom::network::upperlayer::scp scp;
       dicom::network::dimse::dimse_pm_manager dimse_pm;
 
+      std::unique_ptr<storage_scu_thread> storage_operation;
       std::unique_ptr<std::thread> storage_thread;
 
       std::function<void(queryretrieve_scp*, dicom::data::dataset::commandset_data, std::unique_ptr<dicom::data::dataset::iod>)> handler;
