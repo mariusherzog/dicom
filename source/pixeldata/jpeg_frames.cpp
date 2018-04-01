b7e1855a05826dac7703dfbdf600a899f8ff6ae0
Marius Herzog <info@mariusherzog.net>
2018-04-01 15:21:13 +0200

    added dedicated class to retrieve compressed frame information for jpeg transfer syntaxes

---
+++ source/pixeldata/jpeg_frames.cpp
@@ -0,0 +1,6 @@
+#include "jpeg_frames.hpp"
+
+jpeg_frames::jpeg_frames()
+{
+
+}
---
+++ source/pixeldata/jpeg_frames.hpp
@@ -0,0 +1,11 @@
+#ifndef JPEG_FRAMES_HPP
+#define JPEG_FRAMES_HPP
+
+
+class jpeg_frames
+{
+   public:
+      jpeg_frames();
+};
+
+#endif // JPEG_FRAMES_HPP
