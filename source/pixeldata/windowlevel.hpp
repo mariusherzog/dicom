963d412dd34fb98d00efbb996d934880fce29f74
Marius Herzog <info@mariusherzog.net>
2018-06-05 20:09:17 +0200

    refactor windowlevel class and added linear_exact w/l method

--- source/pixeldata/windowlevel.hpp
+++ source/pixeldata/windowlevel.hpp
@@ -12,6 +12,38 @@
 #include "pixeltype.hpp"
 #include "rgb.hpp"
 
+/**
+ * @brief windowlevel_linear performs a linear mapping as given by the window
+ *        / level parameters. This is the default.
+ * @param window window
+ * @param level level
+ * @param value value to which to apply the w/l function
+ * @return mapped value
+ */
+double windowlevel_linear(double window, double level, double value);
+
+/**
+ * @brief windowlevel_sigmoid performs a sigmoidal mapping with the specified
+ *        window / level parameters
+ * @param window window
+ * @param level level
+ * @param value value to which to apply the w/l function
+ * @return transformed value.
+ */
+double windowlevel_sigmoid(double window, double level, double value);
+
+/**
+ * @brief windowlevel_linearexact performs an exact linear mapping with the
+ *        specified window / level parameters, ie. there is no special handling
+ *        which result in thresholding for window = 1
+ * @param window window
+ * @param level level
+ * @param value value to which to apply the w/l function
+ * @return transformed value
+ */
+double windowlevel_linearexact(double window, double level, double value);
+
+
 class windowlevel : public boost::static_visitor<pixeltype>
 {
     private:
@@ -31,13 +63,29 @@ class windowlevel : public boost::static_visitor<pixeltype>
            window = std::stod(str_window);
         }
 
-        bool is_sigmoid_function()
+
+        std::function<double(double, double, double)>
+        get_windowlevel_function_for_set()
         {
            if (set.find({0x0028, 0x1056}) != set.end()) {
               std::string function = set[{0x0028, 0x1056}].value<dicom::data::attribute::VR::CS>();
-              return function == "SIGMOID ";
+              if (function == "SIGMOID ") {
+                 return windowlevel_sigmoid;
+              } else if (function == "LINEAR_EXACT") {
+                 return windowlevel_linearexact;
+              }
+           }
+           return windowlevel_linear;
+        }
+
+        void get_window_level(double& window, double& level)
+        {
+           if (have_window_level()) {
+              window_level_from_set(window, level);
            } else {
-              return false;
+              /// @todo estimate
+              window = 1000;
+              level = 300;
            }
         }
 
@@ -50,39 +98,13 @@ class windowlevel : public boost::static_visitor<pixeltype>
         template <typename T, typename V = typename T::value_type>
         typename std::enable_if<std::is_integral<V>::value, pixeltype>::type operator()(T data)
         {
-           double level;
            double window;
-           if (have_window_level()) {
-              window_level_from_set(window, level);
-           } else {
-              // todo estimate
-              window = 1000;
-              level = 300;
-           }
+           double level;
+           get_window_level(window, level);
 
-           //constexpr std::size_t offset = std::is_signed<V>::value ? std::numeric_limits<V>::max() : 0;
-
-           auto offset = 0;
-
-           if (!is_sigmoid_function()) {
-              for (auto& v : data) {
-                 double norm;
-                 if (v <= level-0.5-window/2.0) {
-                    //norm = offset;
-                    norm = 0.0;
-                 } else if (v > level-0.5+window/2.0) {
-                    //norm = offset+255;
-                    norm = 1.0;
-                 } else {
-                    norm = ((v-level-0.5) / (window-1.0)) + 0.5;
-                 }
-                 //v = 255.0*norm - offset;
-                 v = 255 * norm - offset;
-              }
-           } else {
-              for (auto& v : data) {
-                 v = 255.0 / (1+ std::exp(-4*(v-level)/window ));
-              }
+           auto fun = get_windowlevel_function_for_set();
+           for (auto& v : data) {
+              v = fun(window, level, v);
            }
 
            return data;
@@ -94,6 +116,43 @@ class windowlevel : public boost::static_visitor<pixeltype>
            return data;
         }
 };
+
+
+
+double windowlevel_linear(double window, double level, double value)
+{
+   double norm;
+   if (value <= level-0.5-window/2.0) {
+      //norm = offset;
+      norm = 0.0;
+   } else if (value > level-0.5+window/2.0) {
+      //norm = offset+255;
+      norm = 1.0;
+   } else {
+      norm = ((value-level-0.5) / (window-1.0)) + 0.5;
+   }
+   // 255.0 * norm - offset;
+   return 255 * norm;
+}
+
+double windowlevel_sigmoid(double window, double level, double value)
+{
+   return 255.0 / (1+ std::exp(-4*(value-level)/window));
+}
+
+double windowlevel_linearexact(double window, double level, double value)
+{
+   window = std::max(window, 1.0);
+   double norm;
+   if (value <= level-window/2.0) {
+      norm = 0.0;
+   } else if (value > level+window/2.0) {
+      norm = 1.0;
+   } else {
+      norm = ((value-level) / (window));
+   }
+   return 255 * norm;
+}
 
 
 #endif // WINDOWLEVEL_HPP
