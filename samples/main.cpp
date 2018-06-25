4fd46fe847bc334b4c4538dac634a2bd83f369ef
Marius Herzog <info@mariusherzog.net>
2018-06-25 21:13:30 +0200

    presentation lut module: consider photometric interpretation and presentation lut shape

--- samples/main.cpp
+++ samples/main.cpp
@@ -42,7 +42,7 @@ int main()
 //      {
          dataset::iod dicm;
          dicom::filesystem::dicomfile file(dicm, dict);
-         std::fstream outfile("009004-LUTSHPSG.dcm", std::ios::in | std::ios::binary);
+         std::fstream outfile("RG1_J2KR", std::ios::in | std::ios::binary);
          outfile >> file;
          std::cout << file.dataset() << std::flush;
 
--- source/pixeldata/presentation.hpp
+++ source/pixeldata/presentation.hpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <iostream>
 #include <limits>
+#include <string>
 
 #include <boost/variant.hpp>
 
@@ -19,11 +20,35 @@ class presentation : public boost::static_visitor<std::vector<O>>
    private:
       dicom::data::dataset::dataset_type set;
 
+      bool is_inverse_lut_shape() const
+      {
+         using namespace dicom::data::attribute;
+         if (contains_tag(set, {0x2050, 0x0020})) {
+            std::string lut_shape = set.at({0x2050, 0x0020}).value<VR::CS>();
+            return lut_shape == "INVERSE ";
+         }
+         return false;
+      }
+
+      bool is_monochrome1() const
+      {
+         using namespace dicom::data::attribute;
+         if (contains_tag(set, {0x0028, 0x0004})) {
+            std::string photo_interpretation = set.at({0x0028, 0x0004}).value<VR::CS>();
+            return photo_interpretation == "MONOCHROME1 ";
+         }
+         return false;
+      }
+
+      bool needs_inversion() const
+      {
+         return is_monochrome1() ^ is_inverse_lut_shape();
+      }
+
    public:
       presentation(dicom::data::dataset::dataset_type set):
          set {set}
       {
-         // for now assume Presentation LUT Shape (2050,0020) is always LINEAR
       }
 
 
@@ -34,6 +59,9 @@ class presentation : public boost::static_visitor<std::vector<O>>
          resized_data.resize(data.size());
          for (int i=0; i<data.size(); ++i) {
             resized_data[i] = RNG*(data[i]/(RNG*1.0));
+            if (needs_inversion()) {
+               resized_data[i] = RNG - resized_data[i];
+            }
          }
          return resized_data;
       }
@@ -45,6 +73,9 @@ class presentation : public boost::static_visitor<std::vector<O>>
          resized_data.resize(data.size());
          for (int i=0; i<data.size(); ++i) {
             resized_data[i] = RNG*(data[i]/(RNG*1.0));
+            if (needs_inversion()) {
+               resized_data[i] = RNG - resized_data[i];
+            }
          }
          return resized_data;
       }
